19e008153c24dc3ce50d0a6dd832ac80d09a07d1
evilkashmir.kzn@gmail.com <evilkashmir.kzn@gmail.com>
2020-03-21 20:29:13 +0300

    Add new methods and descriptions
    Add description to Monomial constructor, compareTo, toString. Update Monomial constructor. Add method getData(). Add description to new methods and update descriptions in old methods.

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,22 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         Polinom3 x = new Polinom3("src\\Test");
-        System.out.println(x.toString());
-        x.insert(5,5,5,5);
+        /*x.insert(5,5,5,5);
         System.out.println(x.toString());
         x.insert(5,0,0,0);
         System.out.println(x.toString());
         x.insert(5234,0,0,0);
         System.out.println(x.toString());
-
-
-
+        x.delete(2,3,4);
+        System.out.println(x.toString());
+        x.derivative(1);
+        System.out.println(x.toString());
+        x.add(new Polinom3("src\\Test2"));*/
+        System.out.println(x.toString());
+        /*x.delete(2,3,4);
+        System.out.println(x.toString());
+        x.add(new Polinom3("src\\Test"));
+        System.out.println(x.toString());*/
     }
 
 }
--- src/Polinom3.java
+++ src/Polinom3.java
@@ -7,6 +7,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Polinom3 {
@@ -22,13 +23,29 @@ public class Polinom3 {
         private int y;
         private int z;
 
+        /**
+         * Constructor, which build Monomial with coefficient and powers of variables
+         * @param c - coefficient of Monomial
+         * @param x - power of X
+         * @param y - power of Y
+         * @param z - power of Z
+         */
         public Monomial(int c, int x, int y, int z) {
-            this.c = c;
-            this.x = x;
-            this.y = y;
-            this.z = z;
+            if (c != 0) {
+                this.c = c;
+                this.x = x;
+                this.y = y;
+                this.z = z;
+            }
         }
 
+        /**
+         * Method, which compares two monomials
+         * @param o - another Monomial
+         * @return -1 if power of source monomial larger than input monomial,
+         *          1 if power of source monomial less than input monomial,
+         *          0 if two monomials equal
+         */
         @Override
         public int compareTo(Monomial o) {
             if(this.getX() - o.getX() > 0){
@@ -58,6 +75,10 @@ public class Polinom3 {
             }
         }
 
+        /**
+         * Method, which return string implementation of monomial
+         * @return string implementation of monomial
+         */
         @Override
         public String toString() {
             String xx = "";
@@ -81,7 +102,7 @@ public class Polinom3 {
             if(c == -1){
                 return "-" + xx + "*" + yy + "*" + zz;
             }
-            return (c == 1  ? "" : c) + (x == 0 ? "": xx) +  (y == 0 ? "": "*" + yy) + (z == 0 ? "": "*" + zz );
+            return (c == 1  ? " " : c) + (x == 0 ? "": xx) +  (y == 0 ? "": "*" + yy) + (z == 0 ? "": "*" + zz + " ");
         }
 
         public int getC() {
@@ -99,12 +120,13 @@ public class Polinom3 {
         public int getZ() {
             return z;
         }
+
     }
 
     /**
      * Constructor , which builds a list based on a polynomial specified by
      * a list of coefficients and powers in a text file
-     * @param filename
+     * @param filename - input text file
      * @throws FileNotFoundException
      */
     public Polinom3(String filename) throws FileNotFoundException {
@@ -118,15 +140,15 @@ public class Polinom3 {
     }
 
     /**
-     * Method , which inserts new monomial in polinom.If in polinom already exist monomial with same degrees , replaces it.
-     * @param coeff
-     * @param deg1
-     * @param deg2
-     * @param deg3
+     * Method, which inserts new monomial in polinom.If in polinom already exist monomial with same degrees , replaces it.
+     * @param coeff - coefficient
+     * @param deg1 - power of X
+     * @param deg2 - power of Y
+     * @param deg3 - power of Y
      * @throws Exception
      */
     public void insert(int coeff, int deg1, int deg2, int deg3) throws Exception {
-        if(deg1 < 0 || deg2 < 0 || deg3 < 0) {
+        if (deg1 < 0 || deg2 < 0 || deg3 < 0) {
             throw new Exception("Invalid Input!");
         }
         Monomial mon = new Monomial(coeff,deg1,deg2,deg3);
@@ -143,16 +165,47 @@ public class Polinom3 {
         data.add(mon);
     }
 
+    /**
+     * Method, which deletes some monomial with some degrees at Polinom
+     * @param deg1 - power of X
+     * @param deg2 - power of Y
+     * @param deg3 - power of Z
+     * @throws NoSuchElementException
+     */
     public void delete(int deg1, int deg2, int deg3){
-        //TODO method , which deletes some monomial with some degrees at Polinom
-    }
-
-    public void add(Polinom3 p){
-        //TODO method , which adds another Polinom
+        Monomial mon = new Monomial(1, deg1, deg2, deg3);
+        if (deg1 < 0 || deg2 < 0 || deg3 < 0) {
+            throw new IllegalArgumentException("Invalid Input!");
+        }
+        for (int i = 0; i < data.size(); i++) {
+            if (mon.compareTo(data.get(i)) == 0) {
+                data.remove(i);
+                return;
+            }
+        }
+        throw new NoSuchElementException("Element Not Found!");
     }
 
     /**
-     * Method , which calculates the value in some point(x,y,z)
+     * Method, which adds another Polinom
+     * @param p - new Polinom
+     */
+    public void add(Polinom3 p) {
+        for(Monomial el : p.getData()){
+            data.add(el);
+        }
+        data.sort(Monomial::compareTo);
+        for (int i = 0; i < data.size() - 1; i++) {
+            if (data.get(i).compareTo(data.get(i + 1)) == 0) {
+                data.get(i).c = data.get(i).c + data.get(i + 1).c;
+                data.remove(i + 1);
+                i--;
+            }
+        }
+    }
+
+    /**
+     * Method, which calculates the value in some point(x,y,z)
      * @param x
      * @param y
      * @param z
@@ -170,26 +223,49 @@ public class Polinom3 {
      * Method which calculate partial derivative at one of three variables
      * @param i - one of three variables{1,2,3}
      */
-    public void derivate(int i){
-        //TODO method which calculate partial derivative at one of three variables
+    public void derivative(int i) {
+        if (i < 1 || i > 3) {
+            throw new IllegalArgumentException("Invalid Input!");
+        }
+        for (int j = 0; j < data.size(); j++ ) {
+            if (i == 1 && data.get(j).x != 0) {
+                data.get(j).c = data.get(j).c * data.get(j).x;
+                data.get(j).x = data.get(j).x - 1;
+            }
+            else if (i == 2 && data.get(j).y != 0) {
+                data.get(j).c = data.get(j).c * data.get(j).y;
+                data.get(j).y = data.get(j).y - 1;
+            }
+            else if (i == 3 && data.get(j).z != 0) {
+                data.get(j).c = data.get(j).c * data.get(j).z;
+                data.get(j).z = data.get(j).z - 1;
+            } else {
+                data.remove(j);
+            }
+        }
     }
 
     /**
-     * Method , which return string implementation of polinom
-     * @return string implementation of polinom
+     * Method, which return string implementation of Polinom
+     * @return string implementation of Polinom
      */
     @Override
     public String toString() {
         StringBuilder res = new StringBuilder();
         for (int i = 0; i < data.size(); i++) {
             if(i != 0){
-                res.append(data.get(i).getC() > 0 ? " +" : " ");
+                res.append(data.get(i).getC() > 0 ? "+" : "");
             }
             res.append(data.get(i));
         }
         return res.toString();
     }
 
-
-
+    /**
+     * Private method to work with foreach
+     * @return data
+     */
+    private ArrayList<Monomial> getData() {
+        return data;
+    }
 }
